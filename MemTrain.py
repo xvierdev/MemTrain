2f9afb896d1278617fc4b1fb984b0ebcfcc9f076
Wesley Xavier <wesley.xvier@gmail.com>
2024-10-03 14:52:39 -0300

    :rocket: feat: mvp game is functional.

---
+++ MemTrain.py
@@ -0,0 +1,48 @@
+# Esse projeto se destina ao treinamento da memória em sequências numéricas e alfanuméricas geradas de forma aleatório ou pré determinda.
+import random
+import os
+import time
+
+ALPHABET = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+points = 0
+difficult = 1
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def gen_word(lenght):
+    if lenght <= len(ALPHABET) and lenght >= 1:
+        letters = list(ALPHABET)
+        random.shuffle(letters)
+        new_word = ''.join(letters[:lenght])
+        return new_word
+    return ''
+
+def increase_dificult():
+    if points % 5 == 0:
+        global difficult
+        difficult += 1
+
+while True:
+    word = gen_word(difficult)
+    print(word)
+
+    time.sleep(1)
+    clear_screen()
+
+    answer = input('Whats is the word? ')
+
+    if answer == word:
+        points += 1
+        increase_dificult()
+        print(f'Thats right! {points=}')
+        time.sleep(1 + difficult / 2)
+        clear_screen()
+    else:
+        print(f'Wrong answare {answer=} =/ {word=}')
+        print(f'Record {points}')
+        points = 0
+        difficult = 1
+        if input('Continue? y/n') == 'n':
+            break
+        clear_screen()
