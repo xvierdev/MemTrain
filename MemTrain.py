7b79192162b535c3b5c32303ac40c314916b317e
Wesley Xavier <wesley.xvier@gmail.com>
2024-10-10 17:34:06 -0300

    ":wrench: chore: Adjusted cooldown to increase difficulty"

--- MemTrain.py
+++ MemTrain.py
@@ -1,15 +1,18 @@
 # Esse projeto se destina ao treinamento da memória em sequências numéricas e alfanuméricas geradas de forma aleatório ou pré determinda.
 import random
-import os
-import time
+from os import system, name
+from time import sleep
 
 ALPHABET = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+DIGITS = ('0','1', '2', '3', '4', '5', '6', '7', '8', '9')
+TIME_DIVISOR = 3
+
 points = 0
 difficult = 1
 
 # limpa a tela
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    system('cls' if name == 'nt' else 'clear')
 
 # cria sequencia aleatória de acordo ao tamanho indicado por lenght
 def gen_word(lenght):
@@ -29,13 +32,13 @@ def increase_dificult():
 # loop principal do jogo
 print('Welcome!')
 print('Please see the first word!')
-time.sleep(2)
+sleep(2)
 
 while True:
     word = gen_word(difficult)
     print(word)
 
-    time.sleep(1 + difficult / 2)
+    sleep(1 + difficult / TIME_DIVISOR)
     clear_screen()
 
     answer = input('Whats is the word? ')
@@ -44,13 +47,13 @@ while True:
         points += 1
         increase_dificult()
         print(f'Thats right! {points=}')
-        time.sleep(1)
+        sleep(1)
         clear_screen()
     else:
         print(f'Wrong answare {answer=} =/ {word=}')
         print(f'Record {points}')
         points = 0
         difficult = 1
-        if input('Continue? y/n') == 'n':
+        if input('Continue? y/n: ') == 'n':
             break
         clear_screen()
